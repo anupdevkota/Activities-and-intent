a0715ac7feac8cf549c4f76fa82335954d68d022
anupdevkota <devkotaanup321@gmail.com>
2021-12-10 21:09:35 +0545

    coding challenge completed

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -4,54 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Button;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG =
-            MainActivity.class.getSimpleName();
-    public static final String EXTRA_MESSAGE =
-            "com.example.twoactivities.extra.MESSAGE";
-    private EditText mMessageEditText;
-    public static final int TEXT_REQUEST = 1;
-    private TextView mReplyHeadTextView;
-    private TextView mReplyTextView;
+    private Button button_one;
+    private Button button_two;
+    private Button button_three;
+    public static final String EXTRA_PASSAGE="com.example.twoactivities.extra.PASSAGE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mMessageEditText = findViewById(R.id.editText_main);
-        mReplyHeadTextView = findViewById(R.id.text_header_reply);
-        mReplyTextView = findViewById(R.id.text_message_reply);
-    }
+        button_one=findViewById(R.id.text_btn_one);
+        button_two=findViewById(R.id.text_btn_two);
+        button_three=findViewById(R.id.text_btn_three);
 
-    public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button clicked!");
-        Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-
-        startActivityForResult(intent, TEXT_REQUEST);
-
-
-    }
-    @Override
-    public void onActivityResult(int requestCode,
-                                 int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == TEXT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-                mReplyTextView.setText(reply);
-                mReplyTextView.setVisibility(View.VISIBLE);
+        button_one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra(EXTRA_PASSAGE,getResources().getString(R.string.passage_one));
+                startActivity(intent);
             }
-        }
+        });
+
+        button_two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra(EXTRA_PASSAGE,getResources().getString(R.string.passage_two));
+                startActivity(intent);
+            }
+        });
+        button_three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra(EXTRA_PASSAGE,getResources().getString(R.string.passage_three));
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -4,35 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
-    public static final String EXTRA_REPLY =
-            "com.example.twoactivities.extra.REPLY";
-    private EditText mReply;
+import org.w3c.dom.Text;
 
+public class SecondActivity extends AppCompatActivity {
+    TextView passage_text;
+    public static final String TAG=SecondActivity.class.getSimpleName();
+    private String PASSAGE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        mReply = findViewById(R.id.editText_second);
-
-
+        passage_text=findViewById(R.id.passage_text);
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = findViewById(R.id.text_message);
-        textView.setText(message);
+        String message = intent.getStringExtra(MainActivity.EXTRA_PASSAGE);
+        if(message!=null) {
+            passage_text.setText(message);
+        }
+        else{
+            Log.d(TAG,"message not received" );
+        }
 
-    }
-
-    public void returnReply(View view) {
-        String reply = mReply.getText().toString();
-        Intent replyIntent = new Intent();
-        replyIntent.putExtra(EXTRA_REPLY, reply);
-        setResult(RESULT_OK,replyIntent);
-        finish();
     }
 }
